946772ec61a28a4318abadd7d372308da4a774fd
ET-SPARK <86984205+ET-SAPRK@users.noreply.github.com>
2023-08-30 01:17:01 +0300

    edited

--- server/index.js
+++ server/index.js
@@ -1,4 +1,22 @@
+import "dotenv/config";
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+const app = express();
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error Connecting to MongoDB");
+  });
