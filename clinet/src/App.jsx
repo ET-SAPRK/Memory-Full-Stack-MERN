a87de8987622ee72ddb530f3a3eb59a84735a98a
ET-SPARK <86984205+ET-SAPRK@users.noreply.github.com>
2023-08-30 11:23:59 +0300

    edited

--- clinet/src/App.jsx
+++ clinet/src/App.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import memories from "./images/memories.png";
+import { Container, AppBar, Typography, Grow, Grid } from "@mui/material";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+        <img src={memories} alt="icon" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={7}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
 }
 
 export default App;
